d8d89d211ec09e825642d200c1072efe51bffe45
jessekathryn <jessekathrynbarnes@gmail.com>
2020-12-05 17:14:48 -0500

    App
    ivs_demo clone and download into ecommerce branch

---
+++ web-ui/src/config.js
@@ -0,0 +1,12 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+// Enabling USE_MOCK_DATA will not require an eCommerce demo Backend (see serverless\README.md)
+export const USE_MOCK_DATA = true;
+
+// API endpoint for retrieving the products list 
+export const GET_PRODUCTS_API = "";
+
+// Default video stream to play inside the video player
+// Replace this with your own Amazon IVS Playback URL
+export const DEFAULT_VIDEO_STREAM = "https://4da4a22026d3.us-west-2.playback.live-video.net/api/video/v1/us-west-2.298083573632.channel.hdviye1zVPxT.m3u8";
